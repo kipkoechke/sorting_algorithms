1ae3aff4fc3800498b4fdf40fc37f3791a1115de
kipkoechke <chepkwonyke1@gmail.com>
2023-09-25 12:55:12 -0700

    Fixed - Betty style issue

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,7 +22,7 @@ void swap_ints(int *a, int *b)
 
 /**
  * bubble_sort - Sorts an array of integers in ascending order
- * 			using Bubble Sort.
+ *			using Bubble Sort.
  * @array: Array of integers to be sorted.
  * @size: Size of the array.
  *
